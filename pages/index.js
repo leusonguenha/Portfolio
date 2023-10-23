4b09f32b0efe3c06fc37eac618ca422718a7ff2d
Leuso Nguenha <leusojustino@gmail.com>
2023-10-24 00:59:06 +0200

    adicionar menu das paginas

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import Head from "next/head";
+import Menu from "../components/Menu";
+
 function Home() {
-  return <h1>Leuso Nguenha's Site</h1>;
+  return (
+    <div>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewPort" content="inicial-scale=1.0, width=device-width" />
+        <meta title="Leuso - Home" />
+      </Head>
+      <Menu />
+      Leuso Nguenha Site
+      <script src="/custom.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    </div>
+  );
 }
 
 export default Home;
