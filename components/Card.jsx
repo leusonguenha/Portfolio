cd613f6d394cc6ef3c21690a4ea8c60e38acd0b9
Leuso Nguenha <leusojustino@gmail.com>
2024-03-23 15:36:32 +0200

    usando a imagem dom linkedin

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
-import profilePic from "../public/perfil.JPG";
 
 function Card() {
+  const perfil =
+    "https://media.licdn.com/dms/image/C4D03AQEG6U3VAwD2Ng/profile-displayphoto-shrink_800_800/0/1657371024941?e=1716422400&v=beta&t=BdscUsmbiM1-3_8OkAQYbyLkWWjzrmTREqSH-MVaG8E";
   return (
     <div className="card">
-      <Image className="card-image" src={profilePic} alt="Profile Picture" />
+      <Image
+        className="card-image"
+        src={perfil}
+        alt="Profile Picture"
+        width={300}
+        height={300}
+      />
       <h2 className="card-title">Leuso Nguenha</h2>
       <p className="card-text">
         Estudante Universitário que esta aprendendo a programar. Estou fazendo
