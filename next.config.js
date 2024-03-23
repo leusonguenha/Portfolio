47b082b9d22f086ba5b9ad78aca2ca34c5795e30
Leuso Nguenha <leusojustino@gmail.com>
2024-03-23 21:32:08 +0200

    add configuracao da foto de perfil

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+        port: "",
+        pathname:
+          "/dms/image/C4D03AQEG6U3VAwD2Ng/profile-displayphoto-shrink_800_800/0/1657371024941/**",
+      },
+    ],
+  },
+};
