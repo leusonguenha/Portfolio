6874547b441b9e1511bdc5985c4df63c9361fa5b
Leuso Nguenha <leusojustino@gmail.com>
2024-03-23 21:56:01 +0200

    add `script` ajax

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -8,11 +8,16 @@ function Home() {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewPort" content="inicial-scale=1.0, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        ></link>
         <meta title="Leuso - About" />
       </Head>
       <Menu />
-
       <Footer />
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+      <script src="custom.js"></script>
     </div>
   );
 }
