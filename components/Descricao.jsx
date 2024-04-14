27dd256b2a8bd3bc7e46ba743b0e123a48c304ed
Leuso Nguenha <leusojustino@gmail.com>
2024-04-14 19:04:43 +0200

    add descricao e habilidade

---
+++ components/Descricao.jsx
@@ -0,0 +1,13 @@
+function Descricao() {
+  return (
+    <>
+      <div id="main">
+        <div className="main-content-desc">
+          <h1>Leuso Nguenha</h1>
+          <p>Software Developer</p>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Descricao;
---
+++ components/Habilidades.jsx
@@ -0,0 +1,37 @@
+function Habilidades() {
+  return (
+    <>
+      <div className="main-habilidade">
+        <h1>Habilidades Profissionais</h1>
+        <div className="habilidade-list">
+          <ul>
+            <li>
+              <i class="fa-brands fa-html5"></i>
+            </li>
+            <li>
+              <i class="fa-brands fa-css3-alt"></i>
+            </li>
+            <li>
+              <i class="fa-brands fa-js"></i>
+            </li>
+            <li>
+              <i class="fa-brands fa-react"></i>
+            </li>
+            <li>
+              <i class="fa-brands fa-java"></i>
+            </li>
+
+            <li>
+              <i class="fa-brands fa-git-alt"></i>
+            </li>
+
+            <li>
+              <i class="fa-brands fa-node"></i>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Habilidades;
